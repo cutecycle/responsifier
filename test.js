d47b25b73634a70859691861db143a625441a4bd
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2022-04-07 23:43:15 -0400

    asdf

--- test.js
+++ test.js
@@ -5,12 +5,17 @@ var pageWidth = document.body.scrollWidth
 var viewportWidth = window.innerWidth
 var tooWide = (pagewidth > viewportWidth)
 
-const userAgent = new UserAgent({ deviceCategory: 'mobile' })
+const mobileUserAgent = new UserAgent({ deviceCategory: 'mobile' })
 
 const saveOffender = function(uri){
 
 }
 const makeitfit = function () { 
+    chrome.runtime.sendMessage({
+        type: 'setUA',
+        ua: mobileUserAgent.
+    });
+
 	
 	
   
