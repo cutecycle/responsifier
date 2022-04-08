8226eec75c5d8689fa8f3e307b231dc0478bd4cc
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2022-04-07 23:40:49 -0400

    asdf

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
+var useragent = require('useragent');
+import UserAgent from 'user-agents';
+
 var pageWidth = document.body.scrollWidth
 var viewportWidth = window.innerWidth
 var tooWide = (pagewidth > viewportWidth)
+
+const userAgent = new UserAgent({ deviceCategory: 'mobile' })
+
+const saveOffender = function(uri){
+
+}
 const makeitfit = function () { 
+	
+	
   
 }
